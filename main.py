db50aa249e5fba2719f05b5ff60a5a368ec779a0
Girish Vijayasankar <116815901+OddGire@users.noreply.github.com>
2023-01-23 14:27:22 +0000

    Added Tkinter and Simple Version of Home Screen

--- main.py
+++ main.py
@@ -1,4 +1,31 @@
-# Import modules
+import tkinter as tk
+# from PIL import ImageTk, Image
+playPressed = False
+settingsPressed = False
+
+window = tk.Tk()
+window.title("Rock, Paper, Scissors Cell Simulator")
+window.geometry("300x300")
+
+hello = tk.Label(text="Rock, Paper, Scissors Cell Simulator")
+hello.pack()
+play = tk.Button(text="Play", command=playPressed)
+play.pack()
+settings = tk.Button(text="Settings", command=settingsPressed)
+settings.pack()
+# img = ImageTk.PhotoImage(Image.open("image.jpg"))
+
+if playPressed:
+  bg = 'red'
+  
+if settingsPressed:
+  settingsPressed = True
+
+# if playPressed:
+#   window.configure(bg='red')
+tk.mainloop()
+
+# Import
 import random
 
 # Create board class
@@ -14,10 +41,10 @@ map3 = Board(9,10,11,12)
 map4 = Board(13,14,15,16)
 map5 = Board(17,18,19,20)
 
-maps = [map1.original, map2.original, map3.original, map4.original, map5.original]
+maps = [map1, map2, map3, map4, map5]
 map = random.choice(tuple(maps))
 # Create a function to randomly load in a map
 def loadMap():
   print(map)
 
-loadMap()
+# loadMap()
